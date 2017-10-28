2a1cbbf8cc56ac3aff82f2430407c7040c96400b
CycloPi <venti3d@gmail.com>
2017-10-28 13:17:47 +0200

    Evolución del sniper al leucocito

---
+++ GDsingleton.gd
@@ -0,0 +1,14 @@
+extends Node
+var tiemposegundos = 0
+var balikas
+var distaCentro  = 0
+var distanciaPayo
+var posicionPayo = Vector3()
+var puntos = 0
+var vida = 100
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	print("aquiStoy")
+	pass
